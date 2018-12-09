44824de9735ef43d086385dbaad91ecadb4b4206
fmersseman <fmersseman@gmail.com>
2018-12-09 22:31:00 +0100

    step 1: my first controller with twig

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     *
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function index(
+        Environment $twig
+    )
+    {
+        return new Response($twig->render('default/index.html.twig'));
+    }
+}
